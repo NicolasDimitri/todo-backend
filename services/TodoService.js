2036683223c7a2fe465961cb8fbaeedfde2ae954
Nicolas <nicolasdodimitri@gmail.com>
2022-06-27 20:43:38 -0300

    todo services

---
+++ services/TodoService.js
@@ -0,0 +1,29 @@
+const { Todo } = require('../models');
+
+const getAllTodos = async () => {
+    const todos = await Todo.findAll();
+    return todos;
+};
+
+const deleteTodo = async (id) => {
+    const todo = await Todo.destroy({
+        where: {
+            id
+        }
+    });
+    return todo;
+};
+
+const updateTodo = async (id, todo) => {
+    const updatedTodo = await Todo.update(todo, {
+        where: {
+            id
+        }
+    });
+    return updatedTodo;
+};
+
+module.exports = {
+    getAllTodos,
+    deleteTodo
+};
